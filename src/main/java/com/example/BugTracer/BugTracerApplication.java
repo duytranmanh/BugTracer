38f92430fc2bc7c822c4f379bebaff7dc4e1afdf
Duy Tran <dmtran@wisc.edu>
2024-05-13 07:10:07 +0700

    Initial commit

---
+++ src/main/java/com/example/BugTracer/BugTracerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BugTracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BugTracer/BugTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BugTracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
